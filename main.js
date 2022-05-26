4294515d4f215ed8d463bf29b1f7fd926cb60b08
ebukanas <edvinas.bukanas@gmail.com>
2022-05-26 17:38:30 +0300

    updated

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ var nextBtn = document.querySelector('.buttons button:last-child')
 var firstImage = document.querySelector('.avatar-images #tanya_img')
 var secondImage = document.querySelector('.avatar-images #john_img')
 
+var contentJohn = document.querySelector('.content .john')
+var contentTanya = document.querySelector('.content .tanya')
+
 prevBtn.addEventListener('click', function() {
     firstImage.style.display = 'none';
     secondImage.style.display = 'block';
+    contentTanya.style.display = 'none';
+    contentJohn.style.display = 'block';
 })
 
 nextBtn.addEventListener('click', function() {
     secondImage.style.display = 'none';
     firstImage.style.display = 'block';
+    contentJohn.style.display = 'none';
+    contentTanya.style.display = 'block';
 })
